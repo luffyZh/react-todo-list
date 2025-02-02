e3529f6478fd93f9f4b90ed8c99153b65ce446f2
luffyZh <zhoudeyou945@126.com>
2025-02-02 13:41:26 +0800

    :tada: feat: 适配 github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 适配 github pages
+  base: '/react-todo-list/',
+  build: {
+    // 适配 github pages
+    outDir: 'docs'
+  }
 })
